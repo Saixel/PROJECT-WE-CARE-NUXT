589e1d255b8a2bc3a824a15a9ce5f92e42d676b6
Augusto Hdez <augustojhhdez@gmail.com>
2021-07-21 09:42:31 +0100

    test

--- pages/profiles/_id.vue
+++ pages/profiles/_id.vue
@@ -1,8 +1,9 @@
-<template>
+<template>  
   <v-container v-if="content">
-    <v-row>
-      <v-col>{{content}}</v-col>
+    <v-row class="d-flex">
+      <v-col style="background-color: white; height: 85vh; border-radius: 5px; text-align: center" class="elevation-4 pa-10"><h1>2. Ordena las palabras</h1></v-col>
     </v-row>
+    {{content}}
   </v-container>
 </template>
 
