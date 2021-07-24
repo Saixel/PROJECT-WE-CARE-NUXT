363d1af79e8a40eafb61f1d54ee0ee463805eb62
Alexis Guanche <silver.mugiwara@gmail.com>
2021-07-24 17:36:54 +0100

    Refactoring cards

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,16 +2,16 @@
   <v-footer color="primary lighten-1" dark padless>
     <v-row justify="center" no-gutters>
       <v-col class="text-center">
-        {{ new Date().getFullYear() }} — <strong>WE CARE</strong>
-        <v-btn
+        <!-- {{ new Date().getFullYear() }} — <strong>WE CARE</strong> -->
+        <nuxt-link
           v-for="(link, idx) in links"
           :key="idx"
-          text
-          rounded
-          class="my-2"
+          :to="{ path: link.path }"
         >
-          {{ link }}
-        </v-btn>
+          <v-btn text rounded class="my-2">
+            {{ link.name }}
+          </v-btn>
+        </nuxt-link>
       </v-col>
       <!-- <v-spacer></v-spacer> -->
       <!-- <v-col class="my-4 text-right mr-8" cols="2">
@@ -24,7 +24,25 @@
 <script>
 export default {
   data: () => ({
-    links: ['home', 'about us', 'team', 'services', 'contact Us'],
-  })
+    // links: ['home', 'about us', 'team', 'services', 'contact Us'],
+    links: [
+      {
+        name: 'we care',
+        path: '/',
+      },
+      {
+        name: 'equipo',
+        path: '/team',
+      },
+      {
+        name: 'proyecto',
+        path: '/project',
+      },
+      {
+        name: 'Contacto',
+        path: '/',
+      },
+    ],
+  }),
 }
 </script>
